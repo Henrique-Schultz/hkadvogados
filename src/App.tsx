3d33ab73923de75c6055fda8049031a5b220bc89
Henrique-Schultz <valente.schultz@gmail.com>
2026-04-24 02:29:17 -0300

    fixing bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import EntreEmContato from "./components/sections/EntreEmContato";
 import Header from "./components/sections/Header";
 import QuemSomos from "./components/sections/QuemSomos";
 import Video from "./components/Video";
@@ -7,7 +6,6 @@ function App() {
   return (
     <div className="flex flex-col bg-[var(--dark-blue)] items-center justify-start font-jakarta">
       <Header />
-      {/* <EntreEmContato /> */}
       <Video />
       <QuemSomos />
       <div className="w-full p-4 gap-4 flex items-center justify-center bg-gray-800">
